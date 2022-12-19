9b7b3af6338cc844254f9f5eeb51d1aeb6f52ac9
NicoVIII <nico@nicoviii.net>
2022-12-19 19:49:47 +0000

    Adjust tests
    Sadly the installsAfter property is still not considered, when node feature is given a version...
    Therefore I removed it for now from the ubuntu-with-node scenario.

--- test/pnpm/ubuntu-latest.sh
+++ test/pnpm/ubuntu-latest.sh
@@ -6,7 +6,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "pnpm" pnpm -v
+check "pnpm -v" pnpm -v
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
--- test/pnpm/ubuntu-with-node.sh
+++ test/pnpm/ubuntu-with-node.sh
@@ -4,9 +4,23 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
+check_node() {
+    VERSION=$(node -v)
+    echo "$VERSION"
+    if [[ $VERSION == v16.* ]] # * is used for pattern matching
+    then
+        echo "Version is 16."
+        return 0
+    else
+        echo "Version is NOT 16!"
+        return 1
+    fi
+}
+
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "pnpm -v" pnpm -v
+check "node -v" check_node
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
