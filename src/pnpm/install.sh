2c12633d991666dbc5cd8ef49e6c952312671c85
yf-yang <36890796+yf-yang@users.noreply.github.com>
2022-12-19 13:24:24 +0000

    add ca-certificates

--- src/pnpm/install.sh
+++ src/pnpm/install.sh
@@ -17,34 +17,33 @@ if [ "$(id -u)" -ne 0 ]; then
     root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
 	exit 1
 fi
-
 check_packages() {
-	# This is part of devcontainers-contrib script library
-	# source: https://github.com/devcontainers-contrib/features/tree/v1.1.8/script-library
-	if ! dpkg -s "$@" >/dev/null 2>&1; then
-		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-			echo "Running apt-get update..."
-			apt-get update -y
-		fi
-		apt-get -y install --no-install-recommends "$@"
-	fi
+    # This is part of devcontainers-contrib script library
+    # source: https://github.com/devcontainers-contrib/features/tree/v1.2.0/script-library
+  if ! dpkg -s "$@" > /dev/null 2>&1; then
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+      echo "Running apt-get update..."
+      apt-get update -y
+    fi
+    apt-get -y install --no-install-recommends "$@"
+  fi
 }
 
 install_via_npm() {
-	# This is part of devcontainers-contrib script library
-	# source: https://github.com/devcontainers-contrib/features/tree/v1.1.8/script-library
-	PACKAGE=$1
-
-	# install node+npm if does not exists
-	if ! type npm >/dev/null 2>&1; then
-		echo "Installing node and npm..."
-		check_packages curl
-		curl -fsSL https://raw.githubusercontent.com/devcontainers/features/main/src/node/install.sh | $SHELL
-		export NVM_DIR=/usr/local/share/nvm
-		[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-		[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-	fi
-	npm install -g --omit=dev $PACKAGE
+    # This is part of devcontainers-contrib script library
+    # source: https://github.com/devcontainers-contrib/features/tree/v1.2.0/script-library
+    PACKAGE=$1
+    
+    # install node+npm if does not exists
+    if ! type npm >/dev/null 2>&1; then
+        echo "Installing node and npm..."
+        check_packages curl ca-certificates
+        curl -fsSL https://raw.githubusercontent.com/devcontainers/features/main/src/node/install.sh | $SHELL
+        export NVM_DIR=/usr/local/share/nvm
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+        [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+    fi
+    npm install -g --omit=dev $PACKAGE
 }
 
 if [ "$PNPM" != "none" ]; then
