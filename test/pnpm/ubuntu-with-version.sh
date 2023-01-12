1e4b67852d6b71283421b2a6c4ef1071062735c6
Nico Linder <nl@fp.de>
2023-01-12 09:18:10 +0000

    Unset $VERSION after usage
    $VERSION affected the installed node version which caused problems.
    
    Closes #6

---
+++ test/pnpm/ubuntu-with-version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "pnpm -v" pnpm -v
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
